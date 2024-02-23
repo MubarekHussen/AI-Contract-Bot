930d4f9588f71a7d0da51486cd18a7cc157dbbde
MubarekHussen <mubahussen2014@gmail.com>
2024-02-24 01:19:20 +0300

    feat: creating a chatbot

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatBot from './components/ChatBot/ChatBot';
+
+const App = () => {
+  return (
+    <div className='h-screen flex justify-center items-center'>
+      <ChatBot />
+    </div>
+  );
+};
+
+export default App;
